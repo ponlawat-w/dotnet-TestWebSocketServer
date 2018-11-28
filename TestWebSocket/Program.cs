9a1dec9f2225930e794312973ac58b9a8ee522aa
ponlawat-w <ponlawat_w@cmu.ac.th>
2018-11-28 10:17:50 +0700

    Initial Commit

--- TestWebSocket/Program.cs
+++ TestWebSocket/Program.cs
@@ -19,6 +19,7 @@ namespace TestWebSocket
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://localhost:5000", "http://localhost:5001")
                 .UseStartup<Startup>();
     }
 }
--- TestWebSocket/Startup.cs
+++ TestWebSocket/Startup.cs
@@ -9,6 +9,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.WebSockets;
+using System.Net.WebSockets;
+using Microsoft.AspNetCore.Http;
+using System.Threading;
+using System.Text;
 
 namespace TestWebSocket
 {
@@ -24,7 +29,7 @@ namespace TestWebSocket
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,7 +40,50 @@ namespace TestWebSocket
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            //app.UseMvc();
+            app.UseWebSockets(new WebSocketOptions()
+            {
+                KeepAliveInterval = TimeSpan.FromSeconds(120),
+                ReceiveBufferSize = 128
+            });
+
+            app.Use(async (context, next) => {
+                Console.WriteLine(context.Request.Host);
+                if (context.Request.Path == "/ws")
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                        Console.WriteLine("A client connected");
+                        await Echo(context, webSocket);
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 400;
+                    }
+                }
+                else
+                {
+                    await next();
+                }
+            });
+        }
+
+        private async Task Echo(HttpContext context, WebSocket socket)
+        {
+            byte[] buffer = new byte[128];
+            WebSocketReceiveResult result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                string str = Encoding.UTF8.GetString(buffer);
+                Console.WriteLine($"Received: {str}");
+                
+                await socket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(str.ToUpper()), 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+                Console.WriteLine("Sent");
+                result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await socket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+            Console.WriteLine("Closed");
         }
     }
 }
